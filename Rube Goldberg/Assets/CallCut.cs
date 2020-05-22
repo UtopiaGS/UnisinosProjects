5f7ae5942dcc63832b3e602a06a71e3e9dc02fce
ocruznathalia <ocruznathalia@gmail.com>
2020-05-21 21:23:01 -0300

    Trebuchet and Rube Goldberg base projects

---
+++ Rube Goldberg/Assets/CallCut.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallCut : MonoBehaviour
+{
+    bool _hasTriggered=false;
+    public GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hasTriggered=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject==Target && Target!=null) {
+            if (!_hasTriggered)
+            {
+                _hasTriggered = true;
+                Debug.Log("COLIDIU EM "+collision.gameObject.name);
+                if (CameraTeleport.Cut != null)
+                {
+                    CameraTeleport.Cut.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Rube Goldberg/Assets/CameraTeleport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraTeleport : MonoBehaviour
+{
+
+    public List<GameObject> CameraPositions = new List<GameObject>();
+    public static UnityEvent Cut;
+    int currentIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIndex = 0;
+
+        if (Cut == null)
+            Cut = new UnityEvent();
+
+        Cut.AddListener(ChangeToNextCut);
+
+       ChangeToNextCut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeToNextCut() {
+        Debug.Log("Cut camera index "+currentIndex+ " Count > "+ CameraPositions.Count);
+       for (int i = 0; i < CameraPositions.Count; i++)
+        {
+            if (i == currentIndex)
+            {
+                Debug.Log("Ativando camera na pos current index " + i);
+                CameraPositions[i].gameObject.SetActive(true);
+               
+            }
+            else {
+                CameraPositions[i].gameObject.SetActive(false);
+            }
+        }
+        currentIndex += 1;
+    }
+}
