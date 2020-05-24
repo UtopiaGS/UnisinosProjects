0d936fd4776f071fe8fd51b6461c2899110d24a1
ocruznathalia <ocruznathalia@gmail.com>
2020-05-24 16:19:00 -0300

    Ending in rube goldberg proj

---
+++ Rube Goldberg/Assets/StartFireworks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFireworks : MonoBehaviour
+{
+    bool _hasTriggered=false;
+    public GameObject Target;
+    public List<GameObject> GameObjects = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hasTriggered=false;
+
+        for (int i = 0; i < GameObjects.Count; i++)
+        {
+            GameObjects[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject==Target && Target!=null) {
+            if (!_hasTriggered)
+            {
+                for (int i = 0; i < GameObjects.Count; i++)
+                {
+                    GameObjects[i].SetActive(true);
+                }
+            }
+        }
+    }
+}
