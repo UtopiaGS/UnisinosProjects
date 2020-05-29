5191c6cb583e6438863c70132ee7c0caaa9716bc
ocruznathalia <ocruznathalia@gmail.com>
2020-05-28 23:10:43 -0300

    Rube goldber ending, new changers on trebuchet

---
+++ Trebuchet/Trebuchet/Assets/RelaseProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelaseProjectile : MonoBehaviour
+{
+    public GameObject Target;
+    private HingeJoint targetJoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+       targetJoint =  Target.GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Target) {
+            targetJoint.breakForce = 0;
+        }
+    }
+}
