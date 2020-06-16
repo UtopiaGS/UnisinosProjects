4e8b2b7fc7ed3bca00e820a22a6720419cc6d3ba
ocruznathalia <ocruznathalia@gmail.com>
2020-06-16 13:55:48 -0300

    Add some sound effects and track

---
+++ Trebuchet/Trebuchet/Assets/SoundPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayer : MonoBehaviour
+{
+    private AudioSource _source;
+
+    public List<AudioClip> Clips = new List<AudioClip>();
+
+    public static SoundPlayer Instance;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    public void PlayClipId(int i) {
+        if(Clips[i]!=null)
+            _source.PlayOneShot(Clips[i]);
+    }
+
+    public void PlayClip(AudioClip i)
+    {
+            _source.PlayOneShot(i);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Trebuchet/Trebuchet/Assets/StoneTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StoneTrigger : MonoBehaviour
+{
+
+    Rigidbody _body;
+
+    public List<AudioClip> ClipsExplosion = new List<AudioClip>();
+
+    private Vector3 _currentVelocity;
+    private float dot;
+    private Vector3 _previousVelocity;
+
+    UnityEvent _OnHit = new UnityEvent();
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        _OnHit.AddListener(CallRandomExplosion);
+    }
+
+    void CallRandomExplosion() {
+        int i = Random.Range(0, ClipsExplosion.Count - 1);
+        Debug.Log("I is " + i);
+        SoundPlayer.Instance.PlayClip(ClipsExplosion[i]);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _previousVelocity = _currentVelocity;
+        _currentVelocity = _body.velocity;
+        dot = Vector3.Dot(_previousVelocity, _currentVelocity);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bricks") && dot>15)
+        {
+            _OnHit.Invoke();
+        }
+    }
+}
